77301637cb5ea607454c8dcc2f6be3d7481650e0
Nujian Den Mark Meralpis <denmarkmeralpis@gmail.com>
2019-11-10 15:25:49 +0800

    apply multiple filter instead of getting the first filter

--- lib/queryko/filterer.rb
+++ lib/queryko/filterer.rb
@@ -1,11 +1,21 @@
 module Queryko
   module Filterer
+    # def filter_by_filters
+    #   fields.each do |field, filter|
+    #     if field == 'limit' || field == 'page'
+    #       paginate(filter, )
+    #     end
+    #     self.relation = filter.first.call(relation, params[field], self) if params[field]
+    #   end
+    # end
+
     def filter_by_filters
       fields.each do |field, filter|
-        if field == 'limit' || field == 'page'
-          paginate(filter, )
+        paginate(filter) if ['limit', 'page'].include?(field.to_s)
+
+        filter.each do |f|
+          self.relation = f.call(relation, params[field], self) if params[field]
         end
-        self.relation = filter.first.call(relation, params[field], self) if params[field]
       end
     end
 
